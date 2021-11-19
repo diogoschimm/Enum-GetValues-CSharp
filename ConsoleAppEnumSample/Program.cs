ca6844226196d3039c39e80e2b279cf92eecdde8
Diogo Schimmelpfennig <diogo_ti@hotmail.com.br>
2021-11-18 22:26:11 -0400

    Exemplo de uso Enum

---
+++ ConsoleAppEnumSample/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleAppEnumSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tipos = ETipoPessoaList.All;
+
+            foreach (ETipoPessoa item in tipos)
+            {
+                Console.WriteLine($"Tipo: {item}");
+            }
+        }
+    }
+
+    public static class ETipoPessoaList
+    {
+        public static List<ETipoPessoa> All => Enum.GetValues(typeof(ETipoPessoa)).OfType<ETipoPessoa>().ToList();
+    }
+
+    public enum ETipoPessoa
+    {
+        Funcionario,
+        Gerente,
+        Gestor,
+        Programador,
+        Analista,
+        AuxiliarFinanceiro,
+        Financeiro
+    }
+}
